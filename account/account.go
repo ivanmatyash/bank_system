0edf92b31bace32220b9c4003accbdaf6628b796
Ivan Matsiash <imatsiash@infoblox.com>
2018-04-11 14:10:01 +0300

    Added Database infrastucture to account
    Overview:
    - Added store for account in db;
    - Added updating database after Charge and Add operations;

--- account/account.go
+++ account/account.go
@@ -2,17 +2,20 @@ package account
 
 import (
 	"fmt"
+	"log"
 
+	"github.com/ivanmatyash/bank-golang/sqlstore"
 	"github.com/ivanmatyash/bank-golang/transaction"
 	vld "github.com/ivanmatyash/bank-golang/validate"
 )
 
 type Account struct {
-	id      int32
-	balance int64
+	Id       int32 `db:"id"`
+	ClientId int32 `db:"client_id"`
+	Balance  int64 `db:"balance"`
 }
 
-func NewAccount(balance int64) (acc *Account, err error) {
+func NewAccount(clientId int32, balance int64) (acc *Account, err error) {
 	var (
 		tr *transaction.Transaction
 	)
@@ -21,18 +24,41 @@ func NewAccount(balance int64) (acc *Account, err error) {
 	}
 
 	tr.Start()
-	acc = &Account{1, balance}
-	err = acc.validate()
+	acc = &Account{1, clientId, balance}
 	if err = acc.validate(); err != nil {
 		tr.End(false)
 		return nil, err
 	}
+	var nextId int32
+	err = sqlstore.Db.QueryRow("select nextval ('accounts_id_seq')").Scan(&nextId)
+	if err != nil {
+		log.Println(err)
+		tr.End(false)
+		return nil, err
+	}
+	acc.Id = nextId
+
+	query := `
+		INSERT INTO accounts(
+			id,
+			client_id, 
+			balance
+		) VALUES(
+			:id,
+			:client_id, 
+			:balance
+		)`
+
+	_, err = sqlstore.Db.NamedQuery(query, acc)
+
+	if err != nil {
+		log.Println(err)
+		tr.End(false)
+		return nil, err
+	}
+
 	tr.End(true)
 	return acc, nil
-}
-
-func (a *Account) Balance() int64 {
-	return a.balance
 }
 
 func (a *Account) Charge(money int64) (err error) {
@@ -48,9 +74,22 @@ func (a *Account) Charge(money int64) (err error) {
 		tr.End(false)
 		return err
 	}
-	startBalance := a.balance
-	a.balance -= money
-	tr.AddDiffMoney(a.id, a.balance-startBalance)
+	startBalance := a.Balance
+	a.Balance -= money
+
+	query := `
+		UPDATE accounts SET
+			balance = :balance 
+			WHERE id=:id
+			`
+	_, err = sqlstore.Db.NamedExec(query, a)
+	if err != nil {
+		log.Println(err)
+		tr.End(false)
+		return err
+	}
+
+	tr.AddDiffMoney(a.Id, a.Balance-startBalance)
 	tr.End(true)
 	return err
 }
@@ -68,9 +107,22 @@ func (a *Account) Add(money int64) (err error) {
 		tr.End(false)
 		return err
 	}
-	startBalance := a.balance
-	a.balance += money
-	tr.AddDiffMoney(a.id, a.balance-startBalance)
+	startBalance := a.Balance
+	a.Balance += money
+
+	query := `
+		UPDATE accounts SET
+			balance = :balance 
+			WHERE id=:id
+			`
+	_, err = sqlstore.Db.NamedExec(query, a)
+	if err != nil {
+		log.Println(err)
+		tr.End(false)
+		return err
+	}
+
+	tr.AddDiffMoney(a.Id, a.Balance-startBalance)
 	tr.End(true)
 	return err
 }
@@ -90,15 +142,15 @@ func (a *Account) Transfer(idAccount int32, money int64) (err error) {
 	}
 	// TODO: Add check of existing account with idAccount
 	// TODO: Add creating of object
-	startBalance := a.balance
-	a.balance += money
-	tr.AddDiffMoney(a.id, a.balance-startBalance)
+	startBalance := a.Balance
+	a.Balance += money
+	tr.AddDiffMoney(a.Id, a.Balance-startBalance)
 	tr.End(true)
 	return err
 }
 
 func (a *Account) validate() error {
-	return vld.ValidateAccountBalance(a.balance)
+	return vld.ValidateAccountBalance(a.Balance)
 }
 
 func (a *Account) validateMoneyToOperations(money int64) error {
@@ -115,8 +167,8 @@ func (a *Account) validateChargeMoney(money int64) error {
 	if err := a.validateMoneyToOperations(money); err != nil {
 		return err
 	}
-	if a.balance < money {
-		return fmt.Errorf(vld.ErrorAccountCharge, a.id, a.balance, money)
+	if a.Balance < money {
+		return fmt.Errorf(vld.ErrorAccountCharge, a.Id, a.Balance, money)
 	}
 	return nil
 }
