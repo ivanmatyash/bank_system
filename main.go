02e29852111c328e4890fca7d17444cb6183ee14
Ivan Matsiash <imatsiash@infoblox.com>
2018-04-11 14:44:02 +0300

    Upgraded test file

--- main.go
+++ main.go
@@ -2,18 +2,25 @@ package main
 
 import (
 	"fmt"
-
 	"github.com/ivanmatyash/bank-golang/account"
+
+	"github.com/ivanmatyash/bank-golang/client"
+	//"github.com/ivanmatyash/bank-golang/account"
 	"github.com/ivanmatyash/bank-golang/sqlstore"
 )
 
 func main() {
 	err := sqlstore.InitDB()
 	fmt.Println(err)
-	acc1, err := account.NewAccount(50)
+	client1, err := client.NewClient("Ivan", "test@gmail.com", "1010101")
+	fmt.Println("ERROR=", err)
+	acc1, err := account.NewAccount(client1.Id, 50)
 	fmt.Println(acc1, err)
 	err = acc1.Charge(20)
-	err = acc1.Add(35)
-	fmt.Println(acc1.Balance())
+	acc1.Charge(30)
+	acc1.Add(521)
 	fmt.Println(err)
+	//err = acc1.Add(35)
+	//fmt.Println(acc1.Balance())
+	//fmt.Println(err)
 }
