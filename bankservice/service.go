aae6a64c0f29c8787b6f383eef167aaec8bc0697
Ivan Matsiash <imatsiash@infoblox.com>
2018-04-19 12:07:52 +0300

    Added simple implementation of bankServer interface

--- bankservice/service.go
+++ bankservice/service.go
@@ -20,3 +20,11 @@ func (s *bankServer) ListAccounts(ctx context.Context, req *bank.RequestById) (*
 func (s *bankServer) ReadAccount(ctx context.Context, req *bank.RequestById) (*bank.ResponseAccount, error) {
 	return &bank.ResponseAccount{[]*bank.Account{&bank.Account{req.GetId(), 2, 3}}}, nil
 }
+
+func (s *bankServer) ListClients(ctx context.Context, req *bank.RequestById) (*bank.ResponseClient, error) {
+	return &bank.ResponseClient{[]*bank.Client{&bank.Client{1, "ivan", "1", "11"}}}, nil
+}
+
+func (s *bankServer) ReadClient(ctx context.Context, req *bank.RequestById) (*bank.ResponseClient, error) {
+	return &bank.ResponseClient{[]*bank.Client{&bank.Client{1, "ivan", "1", "11"}}}, nil
+}
