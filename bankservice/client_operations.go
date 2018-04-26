83caec1c3374d3d68d3793372a48d8790fcd5456
Ivan Matsiash <imatsiash@infoblox.com>
2018-04-26 18:28:25 +0300

    Implemented TransferMoney()

--- bankservice/client_operations.go
+++ bankservice/client_operations.go
@@ -2,6 +2,7 @@ package bankservice
 
 import (
 	"context"
+	"github.com/ivanmatyash/bank-golang/validate"
 	"log"
 
 	"github.com/ivanmatyash/bank-golang/api"
@@ -58,4 +59,82 @@ func (s *bankServer) GetBalance(ctx context.Context, in *api.RequestAccount) (*a
 		return nil, err
 	}
 	return &api.ResponseMoney{res.Result[0].Balance}, nil
+}
+
+func (s *bankServer) TransferMoney(ctx context.Context, in *api.RequestTransferMoney) (*api.ResponseAccount, error) {
+	transaction, err := s.StartTransaction("Transfer money.")
+	if err != nil {
+		log.Println(err)
+		return nil, err
+	}
+
+	if err := validate.ValidateTransferMoney(in.Money); err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	var (
+		srcAccount *api.Account
+		dstAccount *api.Account
+	)
+
+	res, err := s.ReadAccount(ctx, &api.RequestById{in.Src.Id})
+	if err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+	srcAccount = res.Result[0]
+
+	res, err = s.ReadAccount(ctx, &api.RequestById{in.Dst.Id})
+	if err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+	dstAccount = res.Result[0]
+
+	srcAccount.Balance -= in.Money
+	if err := srcAccount.Validate(); err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	res, err = s.UpdateAccount(ctx, &api.RequestAccount{srcAccount, srcAccount.Id})
+	if err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	dstAccount.Balance += in.Money
+	res, err = s.UpdateAccount(ctx, &api.RequestAccount{dstAccount, dstAccount.Id})
+	if err != nil {
+		log.Println(err)
+		if err := s.EndTransaction(transaction, false); err != nil {
+			return nil, err
+		}
+		return nil, err
+	}
+
+	transaction.DiffMoney[srcAccount.Id] = -1 * in.Money
+	transaction.DiffMoney[dstAccount.Id] = in.Money
+	if err := s.EndTransaction(transaction, true); err != nil {
+		return nil, err
+	}
+
+	return &api.ResponseAccount{[]*api.Account{srcAccount, dstAccount}}, nil
+
 }
--- validate/validate.go
+++ validate/validate.go
@@ -5,6 +5,7 @@ import "fmt"
 var (
 	ErrorAccountNegativeBalance = "Error: Account balance cannot be negative!"
 	ErrorEmptyString            = "string is empty."
+	ErrorNegativeTransfer       = "Error: attempt to transfer negative amount of money (%d)."
 )
 
 func ValidateAccountBalance(balance int64) error {
@@ -19,6 +20,13 @@ func ValidateLengthString(name string, s string, min int, max int) error {
 	l := len(s)
 	if l == 0 && min > 0 {
 		return fmt.Errorf("%s %s", name, ErrorEmptyString)
+	}
+	return nil
+}
+
+func ValidateTransferMoney(money int64) error {
+	if money <= 0 {
+		return fmt.Errorf(ErrorNegativeTransfer, money)
 	}
 	return nil
 }
